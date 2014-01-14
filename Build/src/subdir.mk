2cb249a19535aed08f058d481a9e00ff3add827d
ieuan25 <ieuanr@hotmail.co.uk>
2014-01-14 20:25:31 +0000

    Adding makefiles.

---
+++ Build/src/subdir.mk
@@ -0,0 +1,66 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/Config.cpp \
+../src/DynamicGETResource.cpp \
+../src/DynamicPOSTResource.cpp \
+../src/DynamicResource.cpp \
+../src/Exceptions.cpp \
+../src/Helpers.cpp \
+../src/HttpProcessor.cpp \
+../src/HttpRequest.cpp \
+../src/HttpResponse.cpp \
+../src/Main.cpp \
+../src/MimeTypes.cpp \
+../src/Resource.cpp \
+../src/SockInterface.cpp \
+../src/StringOperations.cpp \
+../src/TCPConnection.cpp 
+
+OBJS += \
+./src/Config.o \
+./src/DynamicGETResource.o \
+./src/DynamicPOSTResource.o \
+./src/DynamicResource.o \
+./src/Exceptions.o \
+./src/Helpers.o \
+./src/HttpProcessor.o \
+./src/HttpRequest.o \
+./src/HttpResponse.o \
+./src/Main.o \
+./src/MimeTypes.o \
+./src/Resource.o \
+./src/SockInterface.o \
+./src/StringOperations.o \
+./src/TCPConnection.o 
+
+CPP_DEPS += \
+./src/Config.d \
+./src/DynamicGETResource.d \
+./src/DynamicPOSTResource.d \
+./src/DynamicResource.d \
+./src/Exceptions.d \
+./src/Helpers.d \
+./src/HttpProcessor.d \
+./src/HttpRequest.d \
+./src/HttpResponse.d \
+./src/Main.d \
+./src/MimeTypes.d \
+./src/Resource.d \
+./src/SockInterface.d \
+./src/StringOperations.d \
+./src/TCPConnection.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"/home/ieuan/workspace/HttpServer/src" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
